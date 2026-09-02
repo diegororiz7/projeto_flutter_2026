48048c23b63aba069768c841bacfeecff7e1acac
diegororiz7 <diegororiz7@gmail.com>
2026-09-01 21:48:36 -0300

    Aula 01-09-2026

--- lib/app_senha.dart
+++ lib/app_senha.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_super_parameters, unused_import, prefer_const_constructors, unused_local_variable
+// ignore_for_file: use_super_parameters, unused_import, prefer_const_constructors, unused_local_variable, avoid_web_libraries_in_flutter, unnecessary_string_interpolations
 
 import 'dart:html';
 import 'dart:math';
@@ -28,8 +28,153 @@ class SenhaStatefull extends StatefulWidget {
 }
 
 class _SenhaStatefullState extends State<SenhaStatefull> {
+  double tamanho = 12;
+  bool maius = true;
+  bool minus = true;
+  bool nums = true;
+  bool simb = true;
+  String senha = 'Informe sua senha';
+  final random = Random();
+
+  void gerarSenha() {
+    String chars = '';
+    if (maius) chars += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (minus) chars += 'abcdefghijklmnopqrstuvwxyz';
+    if (nums) chars += '0123456789';
+    if (simb) chars += '!@#%&*/';
+
+    if (chars.isEmpty) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Selecione ao menos uma opção!')));
+    }
+
+    String s = '';
+
+    for (int i = 0; i < tamanho.round(); i++) {
+      s += chars[random.nextInt(chars.length)];
+    }
+
+    setState(() {
+      senha = s;
+    });
+  }
+
+  String forcaSenha() {
+    if (tamanho < 8) return 'Fraca';
+    if (tamanho < 12) return 'Média';
+    return 'Forte';
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Gerador de senha'), centerTitle: true),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Tamanho: ${tamanho.round()}',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Slider(
+                value: tamanho,
+                min: 4,
+                max: 30,
+                divisions: 26,
+                label: tamanho.round().toString(),
+                onChanged: (value) => setState(() {
+                  tamanho = value;
+                }),
+              ),
+              CheckboxListTile(
+                value: maius,
+                title: Text('Letras maiúsculas'),
+                onChanged: (value) => setState(() {
+                  maius = value!;
+                }),
+              ),
+              CheckboxListTile(
+                value: minus,
+                title: Text('Letras minúscuas'),
+                onChanged: (value) => setState(() {
+                  minus = value!;
+                }),
+              ),
+              CheckboxListTile(
+                value: nums,
+                title: Text('Números'),
+                onChanged: (value) => setState(() {
+                  nums = value!;
+                }),
+              ),
+              CheckboxListTile(
+                value: simb,
+                title: Text('Símbolos'),
+                onChanged: (value) => setState(() {
+                  simb = value!;
+                }),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: gerarSenha,
+                  child: Text('Gerar senha'),
+                ),
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'assets/images/senha.jpg',
+                          width: 50,
+                          height: 50,
+                          fit: BoxFit.contain,
+                        ),
+                        Text('Senha gerada!', style: TextStyle(fontSize: 14)),
+                        SizedBox(height: 10),
+                        SelectableText(
+                          '$senha',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Força: ${forcaSenha()}',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                        SizedBox(height: 15),
+                        SizedBox(
+                          width: double.infinity,
+                          child: OutlinedButton.icon(
+                            icon: Icon(Icons.copy),
+                            label: Text('Copar'),
+                            onPressed: () {
+                              Clipboard.setData(ClipboardData(text: senha));
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text('Senha copiada!')),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 // ignore_for_file: camel_case_types
 
 import 'package:flutter/material.dart';
-//import 'package:projeto_flutter_2026/app_paises.dart';
-import 'package:projeto_flutter_2026/app_contagem.dart';
+import 'package:projeto_flutter_2026/app_senha.dart';
 
 void main() {
-  runApp(ContagemStateless());
+  runApp(SenhaStateless());
 }
