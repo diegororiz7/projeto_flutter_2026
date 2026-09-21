3c06cf2699289200daa0bc81c7bacc6886b025c4
diegororiz7 <diegororiz7@gmail.com>
2026-09-21 19:49:33 -0300

    Aula 21-09-2026

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: camel_case_types
 
 import 'package:flutter/material.dart';
-import 'package:projeto_flutter_2026/filmes_app.dart';
+import 'package:projeto_flutter_2026/receitas_app.dart';
 
 void main() {
-  runApp(FilmesApp());
+  runApp(Receitas_Stateless());
 }
--- lib/receitas_app.dart
+++ lib/receitas_app.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: camel_case_types
+// ignore_for_file: camel_case_types, sort_child_properties_last, unused_local_variable, deprecated_member_use
 
 import 'package:flutter/material.dart';
 
@@ -55,7 +55,7 @@ class _Receitas_StatefullState extends State<Receitas_Statefull> {
       });
       nomeController.clear();
       tempoController.clear();
-      categoriaSelecionada = categorias.first;
+      categoriaSelecionada = 'Brasileira';
     });
   }
 
@@ -92,6 +92,103 @@ class _Receitas_StatefullState extends State<Receitas_Statefull> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Lista de receitas'), centerTitle: true),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: nomeController,
+              decoration: const InputDecoration(
+                labelText: 'Nome da receita',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 15),
+            TextField(
+              controller: tempoController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: 'Tempo da receita (minutos)',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 15),
+            DropdownButtonFormField(
+              decoration: const InputDecoration(
+                labelText: 'Categoria',
+                border: OutlineInputBorder(),
+              ),
+              value: categoriaSelecionada,
+              items: categorias.map((categoria) {
+                return DropdownMenuItem(
+                  value: categoria,
+                  child: Text(categoria),
+                );
+              }).toList(),
+              onChanged: (valor) {
+                setState(() {
+                  categoriaSelecionada = valor!;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton.icon(
+                icon: const Icon(Icons.add),
+                label: Text('Adicionar receita'),
+                onPressed: adicionarReceita,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: receitas.isEmpty
+                  ? const Center(
+                      child: Text(
+                        'Não existem receitas cadastradas',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    )
+                  : ListView.builder(
+                      itemCount: receitas.length,
+                      itemBuilder: (context, index) {
+                        final receita = receitas[index];
+                        return Card(
+                          elevation: 3,
+                          margin: const EdgeInsets.only(bottom: 12),
+                          child: ListTile(
+                            leading: Icon(
+                              obterIcone(receita['nome']),
+                              size: 40,
+                              color: Colors.orange,
+                            ),
+                            title: Text(
+                              receita['nome'],
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Categoria: ${receita['categoria']}'),
+                                Text('Tempo (minutos): ${receita['tempo']}'),
+                              ],
+                            ),
+                            trailing: IconButton(
+                              icon: const Icon(Icons.delete, color: Colors.red),
+                              onPressed: () {
+                                removerReceita(index);
+                              },
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
